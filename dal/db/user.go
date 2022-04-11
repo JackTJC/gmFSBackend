249e43f51600290a4b459b92968b1bddadb8f14d
tianjincai <tianjincai@hotmail.com>
2022-04-11 18:41:47 +0800

    update: add base login

--- dal/db/user.go
+++ dal/db/user.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/JackTJC/gmFS_backend/model"
+	"gorm.io/gorm"
 )
 
 var (
@@ -30,10 +31,13 @@ func (d *userDB) GetUserByName(name string) (*model.UserInfo, error) {
 	var ret []*model.UserInfo
 	err := gormDB.Where("user_name = ?", name).Find(&ret).Error
 	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, ErrUserNotFound
+		}
 		return nil, err
 	}
 	if len(ret) == 0 {
-		return nil, nil
+		return nil, ErrUserNotFound
 	}
 	return ret[0], nil
 }
--- method/user_login.go
+++ method/user_login.go
@@ -2,8 +2,12 @@ package method
 
 import (
 	"context"
+	"errors"
 
+	"github.com/JackTJC/gmFS_backend/dal/db"
+	"github.com/JackTJC/gmFS_backend/logs"
 	"github.com/JackTJC/gmFS_backend/pb_gen"
+	"github.com/JackTJC/gmFS_backend/util"
 )
 
 type UserLoginHandler struct {
@@ -14,11 +18,44 @@ type UserLoginHandler struct {
 
 func NewUserLoginHandler(ctx context.Context, req *pb_gen.UserLoginRequest) *UserLoginHandler {
 	return &UserLoginHandler{
-		ctx: ctx,
-		Req: req,
+		ctx:  ctx,
+		Req:  req,
+		Resp: &pb_gen.UserLoginResponse{},
 	}
 }
 
 func (h *UserLoginHandler) Run() {
+	if err := h.checkParams(); err != nil {
+		logs.Sugar.Errorf("checkParams error:%v", err)
+		h.Resp.BaseResp = &pb_gen.BaseResp{
+			StatusCode: pb_gen.StatusCode_CommonErr,
+		}
+		return
+	}
+	user, err := db.User.GetUserByName(h.Req.GetUserName())
+	if err != nil {
+		logs.Sugar.Errorf("GetUserByName error:%v", err)
+		if err == db.ErrUserNotFound {
+			h.Resp.BaseResp = util.BuildBaseResp(pb_gen.StatusCode_UserNotFound)
+			return
+		}
+		h.Resp.BaseResp = util.BuildBaseResp(pb_gen.StatusCode_CommonErr)
+		return
+	}
+	if user.Password != h.Req.GetPassword() {
+		logs.Sugar.Errorf("auth error, wrong passwd")
+		h.Resp.BaseResp = util.BuildBaseResp(pb_gen.StatusCode_WrongPasswd)
+		return
+	}
+	h.Resp.BaseResp = util.BuildBaseResp(pb_gen.StatusCode_Success)
+}
 
+func (h *UserLoginHandler) checkParams() error {
+	if len(h.Req.GetUserName()) == 0 {
+		return errors.New("empty user name")
+	}
+	if len(h.Req.GetPassword()) == 0 {
+		return errors.New("empty passwd")
+	}
+	return nil
 }
